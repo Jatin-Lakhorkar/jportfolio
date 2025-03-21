4fe83426fa25ac863585b953b1393599d5b7451f
Jatin-Lakhorkar <jatin.lakhorkar@gmail.com>
2025-03-21 20:51:25 +0530

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import "./index.css";
 // Utility function for reusable styles
 const getShadowClass = (darkMode) =>
   darkMode
-    ? "shadow-[0px_0px_15px_rgba(14,165,233,0.7)] bg-gradient-to-br from-sky-950 to-gray-900"
-    : "shadow-[0px_0px_15px_rgba(56,189,248,0.5)] bg-gradient-to-br from-white to-sky-100";
+    ? "shadow-[0px_0px_15px_rgba(14,165,233,0.7)] bg-gradient-to-r from-sky-950 to-gray-900"
+    : "shadow-[0px_0px_10px_theme(colors.sky.400)] bg-gradient-to-r from-white to-sky-300";
 
 // Reusable Badge Component
 const Badge = ({ src, alt, darkMode }) => (
